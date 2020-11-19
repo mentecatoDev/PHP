2c982efb60ecdc5994c52adda70a74bba4b72be2
Paco Ávila <favila@iesromerovargas.com>
2020-11-19 01:00:19 +0100

    Cambia accesos a los ejercicios de la T3 desde DWES-Tema02.html

--- UT02/EjerciciosUT02/T2/19-02.php
+++ UT02/EjerciciosUT02/T2/19-02.php
@@ -17,30 +17,22 @@
 
           // número de dos cifras
           if (( $n >= 10 ) && ( $n < 100 )) {
-            if ( floor($n / 10) == ($n % 10) ) {
-              $capicua = true;
-            }
+            $capicua = (floor($n / 10) == ($n % 10));
           }
 
           // número de tres cifras
           if (( $n >= 100 ) && ( $n < 1000 )) {
-            if ( floor($n / 100) == ($n % 10) ) {
-              $capicua = true;
-            }
+            $capicua = (floor($n / 100) == ($n % 10) );
           }
 
           // número de cuatro cifras
           if (( $n >= 1000 ) && ( $n < 10000 )) {
-            if ((floor( $n / 1000 ) == ($n % 10) ) && ( (floor( $n / 100 ) % 10 )== (floor( $n / 10) % 10 ))) {
-              $capicua = true;
-            }
+            $capicua =  ((floor( $n / 1000 ) == ($n % 10) ) && ( (floor( $n / 100 ) % 10 )== (floor( $n / 10) % 10 )));
           }
 
           // número de cinco cifras
           if ( $n >= 10000) {
-            if ((floor( $n / 10000 ) == ($n % 10) ) && ( ( (floor($n / 1000) % 10) ) == (floor( $n / 10) % 10))) {
-              $capicua = true;
-            }
+            $capicua = (floor( $n / 10000 ) == ($n % 10) ) && ( ( (floor($n / 1000) % 10) ) == (floor( $n / 10) % 10));
           }
 
           if ( $capicua ) {
